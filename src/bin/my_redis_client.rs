5a8949e6e5d238be32e0dd63bf5cc8e28c3fb145
Mathius <info@yogimathius.dev>
2024-08-10 23:12:25 -0700

    chore: get parameters in redis client

--- src/bin/my_redis_client.rs
+++ src/bin/my_redis_client.rs
@@ -2,6 +2,7 @@ use my_redis_client::redis_client::RedisClient;
 
 fn main() {
     let command = std::env::args().nth(1).expect("Usage: my_ping <command>");
+    let parameters: Vec<String> = std::env::args().skip(2).collect();
 
     let addr = "localhost".to_string();
     let port = 6739;
