56f5d3a3889e358117cdbe7075d4608327f3586d
Mathius <info@yogimathius.dev>
2024-08-10 23:46:32 -0700

    feat: add Value struct to redis_client (maybe move?)

--- src/redis_client.rs
+++ src/redis_client.rs
@@ -1,31 +1,60 @@
 use anyhow::Result;
-use std::collections::HashSet;
-use std::net::TcpStream;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 pub struct RedisClient {
     server_address: String,
     port: u16,
-    stream: Option<TcpStream>,
+    stream: TcpStream,
     command_queue: Vec<String>,
     last_response: Option<String>,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    SimpleString(String),
+    BulkString(String),
+    Array(Vec<Value>),
+    NullBulkString,
+}
+
+impl Value {
+    pub fn serialize(self) -> String {
+        match self {
+            Value::Array(values) => {
+                let mut serialized = format!("*{}\r\n", values.len());
+                for value in values {
+                    serialized.push_str(&value.serialize());
+                }
+
+                serialized
+            }
+            Value::SimpleString(s) => format!("+{}\r\n", s),
+            Value::BulkString(s) => format!("${}\r\n{}\r\n", s.chars().count(), s),
+            Value::NullBulkString => format!("$-1\r\n"),
+        }
+    }
+}
+
 impl RedisClient {
-    pub fn new(addr: String, port: u16) -> RedisClient {
-        let stream = TcpStream::connect(format!("{addr}:{port}")).unwrap();
+    pub async fn new(addr: String, port: u16) -> RedisClient {
+        let stream = TcpStream::connect(format!("{addr}:{port}")).await.unwrap();
 
         RedisClient {
             server_address: addr.to_string(),
             port: port,
-            stream: Some(stream),
+            stream: stream,
             command_queue: Vec::new(),
             last_response: None,
         }
     }
 
     pub async fn send_command(&mut self, command: String) -> Result<()> {
-        self.stream.write_all(command).await.unwrap();
+        let msg = vec![Value::BulkString(String::from(command))];
+        let payload = Value::Array(msg);
+        self.stream
+            .write_all(payload.serialize().as_bytes())
+            .await
+            .unwrap();
         Ok(())
     }
 }
