1c4c70ce5a569051dbf3208a981b770ad0796a6a
Mathius <info@yogimathius.dev>
2024-08-10 23:19:00 -0700

    chore: add valid commands

--- src/bin/my_redis_client.rs
+++ src/bin/my_redis_client.rs
@@ -4,7 +4,16 @@ fn main() {
     let command = std::env::args().nth(1).expect("Usage: my_ping <command>");
     let parameters: Vec<String> = std::env::args().skip(2).collect();
 
+    if !VALID_COMMANDS.contains(command.as_str()) {
+        eprintln!("Invalid command: {}", command);
+        std::process::exit(1);
+    }
+
+    let formatted_command = format!("{} {}", command, parameters.join(" "));
+
     let addr = "localhost".to_string();
     let port = 6739;
     let redis_client = RedisClient::new(addr, port);
+
+    redis_client.send_command(formatted_command)
 }
--- src/redis_client.rs
+++ src/redis_client.rs
@@ -1,6 +1,45 @@
 use anyhow::Result;
+use lazy_static::lazy_static;
+use std::collections::HashSet;
 use std::net::TcpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+lazy_static! {
+    pub static ref VALID_COMMANDS: HashSet<&'static str> = {
+        let mut m = HashSet::new();
+        m.insert("quit");
+        m.insert("echo");
+        m.insert("ping");
+        m.insert("flushall");
+        m.insert("info");
+        m.insert("set");
+        m.insert("get");
+        m.insert("key");
+        m.insert("type");
+        m.insert("del");
+        m.insert("unlink");
+        m.insert("expire");
+        m.insert("rename");
+        m.insert("llen");
+        m.insert("lrem");
+        m.insert("lindex");
+        m.insert("lpop");
+        m.insert("rpop");
+        m.insert("lpush");
+        m.insert("rpush");
+        m.insert("lset");
+        m.insert("hget");
+        m.insert("hexists");
+        m.insert("hdel");
+        m.insert("hgetall");
+        m.insert("hkeys");
+        m.insert("hlen");
+        m.insert("hmset");
+        m.insert("hset");
+        m.insert("hvals");
+        m
+    };
+}
 
 pub struct RedisClient {
     server_address: String,
@@ -23,7 +62,7 @@ impl RedisClient {
         }
     }
 
-    pub async fn send_command(&mut self, command: String, params: String) -> Result<()> {
+    pub async fn send_command(&mut self, command: String) -> Result<()> {
         self.stream.write_all(command).await.unwrap();
         Ok(())
     }
