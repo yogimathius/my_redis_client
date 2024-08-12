3aa49194701ea34dfc7a1177c3abaf95eea65910
Mathius <info@yogimathius.dev>
2024-08-11 22:52:32 -0700

    wip: remove main.rs and add bin/my_redis_repl.rs

---
+++ src/bin/my_redis_repl.rs
@@ -0,0 +1,22 @@
+use anyhow::Result;
+use my_redis_client::{command_info::COMMANDS, redis_client::RedisClient};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let command = std::env::args().nth(1).expect("Usage: my_ping <command>");
+    let parameters: Vec<String> = std::env::args().skip(2).collect();
+
+    if let Some(command_info) = COMMANDS.get(command.as_str()) {
+        let _ = command_info.validate_args(parameters.clone());
+    } else {
+        eprintln!("Invalid command: {}", command);
+        std::process::exit(1);
+    }
+
+    let addr = "localhost".to_string();
+    let port = 6379;
+    let mut redis_client = RedisClient::new(addr, port).await;
+
+    let _ = redis_client.send_command(command, parameters).await;
+    Ok(())
+}
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use my_redis_client::redis_client::RedisClient;
-
-fn main() {
-    let addr = "localhost".to_string();
-    let port = 6739;
-    let redis_client = RedisClient::new(addr, port);
-}
