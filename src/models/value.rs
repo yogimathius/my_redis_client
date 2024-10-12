4ac6906313610b3e48b343ed84db5c9be2b9e567
Mathius <info@yogimathius.dev>
2024-10-11 22:52:12 -0700

    feat: use utilities to deserialize value

--- src/models/value.rs
+++ src/models/value.rs
@@ -31,37 +31,8 @@ impl Value {
         }
         buf
     }
-
-    pub fn deserialize(input: &str) -> Result<Self> {
-        match input.chars().next() {
-            Some('+') => Ok(Value::SimpleString(input[1..].trim().to_string())),
-            Some('$') => {
-                if input.starts_with("$-1") {
-                    Ok(Value::NullBulkString)
-                } else {
-                    let parts: Vec<&str> = input.split("\r\n").collect();
-                    if parts.len() >= 2 {
-                        Ok(Value::BulkString(parts[1].to_string()))
-                    } else {
-                        Err(anyhow!("Invalid bulk string format"))
-                    }
-                }
-            }
-            Some('*') => {
-                let mut values = Vec::new();
-                let lines: Vec<&str> = input.lines().skip(1).collect();
-                for line in lines {
-                    values.push(Value::deserialize(line)?);
-                }
-                Ok(Value::Array(values))
-            }
-            Some(':') => {
-                let num = input[1..].trim().parse::<i64>()?;
-                Ok(Value::Integer(num))
-            }
-            Some('-') => Ok(Value::Error(input[1..].trim().to_string())),
-            _ => Err(anyhow!("Unknown Redis value type")),
-        }
+    pub fn deserialize(buffer: &mut BytesMut) -> Result<Self> {
+        crate::utilities::parse_message(buffer).map(|(value, _)| value)
     }
 
     pub fn to_string(&self) -> String {
