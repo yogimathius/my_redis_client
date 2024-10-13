cc36f72ca4badd7e1fd8df0fa605b36c076fc156
Mathius <info@yogimathius.dev>
2024-10-12 22:04:03 -0700

    refactor: clean up shell logic

--- src/bin/my_redis_shell.rs
+++ src/bin/my_redis_shell.rs
@@ -1,46 +1,28 @@
-use anyhow::{anyhow, Result};
-use my_redis_client::{
-    command_info::{COMMANDS, DEPRECATED_COMMANDS},
-    log,
-    redis_client::RedisClient,
-};
+use anyhow::Result;
+use my_redis_client::{command_info::CommandInfo, log, redis_client::RedisClient};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 #[structopt(name = "my_redis_cli", about = "A Redis CLI client")]
 struct Cli {
-    /// Redis command to execute
     command: String,
 
-    /// Arguments for the Redis command
     #[structopt(parse(from_str))]
     args: Vec<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cli = Cli::from_args();
+    let Cli { command, args } = Cli::from_args();
+    let command = command.to_uppercase();
 
-    let command = cli.command.to_uppercase();
-
-    if let Some(new_command) = DEPRECATED_COMMANDS.get(command.as_str()) {
-        return Err(anyhow!(
-            "Command '{}' is deprecated. Use '{}' instead.",
-            command,
-            new_command
-        ));
-    }
-
-    let transformed_args = if let Some(command_info) = COMMANDS.get(command.as_str()) {
-        log!("Validating command: {:?}", command);
-        command_info.validate_and_transform_args(cli.args)?
-    } else {
-        return Err(anyhow!("Invalid command: {}", cli.command));
-    };
+    let (validated_command, transformed_args) = CommandInfo::new(&command, args)?;
 
     let mut redis_client = RedisClient::new(None, None).await;
+    let result = redis_client
+        .send_command(validated_command, transformed_args)
+        .await?;
 
-    let result = redis_client.send_command(command, transformed_args).await?;
     log!("{:?}", result);
     Ok(())
 }
